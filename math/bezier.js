8f14cd655b93099c7cf2dd0992d642264cbf9ea4
Singh Payal <118951240+payal-22@users.noreply.github.com>
2025-12-17 14:57:48 +0530

    Update bezier.js

--- math/bezier.js
+++ math/bezier.js
@@ -1,5 +1,8 @@
 import { normalize } from "./vector.js"
 
+
+// For Computing a point on a cubic Bézier curve for parameter t
+// Formula:- B(t) = (1−t)³P₀ + 3(1−t)²tP₁ + 3(1−t)t²P₂ + t³P₃
 export function bezierPoint(t, P0, P1, P2, P3) {
   const u = 1 - t
   const tt = t * t
@@ -18,6 +21,10 @@ export function bezierPoint(t, P0, P1, P2, P3) {
       tt * t * P3.y
   }
 }
+
+// We have to compute the tangent vector using the derivative of the Bézier curve
+// B'(t) = 3(1−t)²(P₁−P₀) + 6(1−t)t(P₂−P₁) + 3t²(P₃−P₂)
+
 
 export function bezierTangent(t, P0, P1, P2, P3) {
   const u = 1 - t
