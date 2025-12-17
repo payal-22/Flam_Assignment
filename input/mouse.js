d003b8cd5ca6f4160bbee39a864c682072159ae1
Singh Payal <118951240+payal-22@users.noreply.github.com>
2025-12-17 14:49:27 +0530

    Update mouse.js

--- input/mouse.js
+++ input/mouse.js
@@ -1,1 +1,14 @@
+export function setupMouse(canvas, target1, target2) {
+  canvas.addEventListener("mousemove", e => {
+    const rect = canvas.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    target1.x = x
+    target1.y = y
+
+    target2.x = canvas.width - x
+    target2.y = canvas.height - y
+  })
+}
 
