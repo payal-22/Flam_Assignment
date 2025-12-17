ff85ceabce25a95840e6a4cfe5ec381aa815338f
Singh Payal <118951240+payal-22@users.noreply.github.com>
2025-12-17 14:50:03 +0530

    Update bezier.js

--- math/bezier.js
+++ math/bezier.js
@@ -1,1 +1,23 @@
+import { normalize } from "./vector.js"
+
+export function bezierPoint(t, P0, P1, P2, P3) {
+  const u = 1 - t
+  const tt = t * t
+  const uu = u * u
+
+  return {
+    x:
+      uu * u * P0.x +
+      3 * uu * t * P1.x +
+      3 * u * tt * P2.x +
+      tt * t * P3.x,
+    y:
+      uu * u * P0.y +
+      3 * uu * t * P1.y +
+      3 * u * tt * P2.y +
+      tt * t * P3.y
+  }
+}
+
+
 
