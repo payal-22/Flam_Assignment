5aa436dd3bbc8b8680a4e5fb640502284b524fb2
Singh Payal <118951240+payal-22@users.noreply.github.com>
2025-12-17 14:52:15 +0530

    Update bezier.js

--- math/bezier.js
+++ math/bezier.js
@@ -19,5 +19,21 @@ export function bezierPoint(t, P0, P1, P2, P3) {
   }
 }
 
+export function bezierTangent(t, P0, P1, P2, P3) {
+  const u = 1 - t
+
+  const v = {
+    x:
+      3 * u * u * (P1.x - P0.x) +
+      6 * u * t * (P2.x - P1.x) +
+      3 * t * t * (P3.x - P2.x),
+    y:
+      3 * u * u * (P1.y - P0.y) +
+      6 * u * t * (P2.y - P1.y) +
+      3 * t * t * (P3.y - P2.y)
+  }
+
+  return normalize(v)
+}
 
 
